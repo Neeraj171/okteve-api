9da732acc059c12c6c8972b7cc02a90299acff5f
Neeraj <110258401+Neeraj171@users.noreply.github.com>
2025-08-04 17:07:44 +0530

    Update playlist.py

--- api/playlist.py
+++ api/playlist.py
@@ -1,1 +1,8 @@
+from scraper import fetch_channels
 
+def handler(request, response):
+    channels = fetch_channels()
+    m3u = "#EXTM3U\n"
+    for ch in channels:
+        m3u += f"#EXTINF:-1,{ch['name']}\n{ch['url']}\n"
+    return response.send(m3u, content_type='audio/x-mpegurl')
